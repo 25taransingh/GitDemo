a5eaa9c851b29e8d42da17118397dc4160bbed9b
Admin <writetotaransingh@gmail.com>
2025-10-04 17:21:46 +0530

    2.1

--- src/test/java/taransinghlearning/Tests/SubmitOrderTest.java
+++ src/test/java/taransinghlearning/Tests/SubmitOrderTest.java
@@ -53,7 +53,7 @@ public class SubmitOrderTest extends BaseTest {
 		String confirmationMsg = confirmationPage.getConfirmationMsg();
 		Assert.assertEquals(confirmationMsg, "THANKYOU FOR THE ORDER.");
 		System.out.println(confirmationMsg);
-		System.out.println("Modified in GitDemoY1");
+		System.out.println("Modified in GitDemoY2");
 //		Thread.sleep(2000);		
 		driver.quit();
 
