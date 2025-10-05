0a0be5be383f474da975f0b545bfc49aa20cdd0b
Admin <writetotaransingh@gmail.com>
2025-10-05 18:54:00 +0530

    New test class is added

---
+++ src/test/java/taransinghlearning/Tests/DemoTest.java
@@ -0,0 +1,10 @@
+package taransinghlearning.Tests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void test1() {
+		System.out.println("after merge changes by git x");
+	}
+	}
